b218ab4612079bb2d0b5e00940ee38bd1a42c3c5
svartfika <svartfika@proton.me>
2025-12-19 23:57:48 +0100

    feat(dag): add env var for duckdb path

--- src/cyano_pipeline/defs/resources.py
+++ src/cyano_pipeline/defs/resources.py
@@ -1,5 +1,10 @@
+import os
+from pathlib import Path
+
 from dagster_dlt import DagsterDltResource
 from dagster_duckdb import DuckDBResource
 
-dlt_res = DagsterDltResource()
-duckdb_res = DuckDBResource(database="ddb.duckdb")
+DUCKDB_PATH: Path = Path(os.getenv("DUCKDB_PATH", "data/warehouse.duckdb")).resolve()
+
+dlt_res: DagsterDltResource = DagsterDltResource()
+duckdb_res: DuckDBResource = DuckDBResource(database=str(DUCKDB_PATH))
