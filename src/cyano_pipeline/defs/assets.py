08f6aef8a01242eb20d959bbe38029f8623d14f4
svartfika <svartfika@proton.me>
2025-12-20 13:04:17 +0100

    feat(dag): initial dim_bathing_waters asset

--- src/cyano_pipeline/defs/assets.py
+++ src/cyano_pipeline/defs/assets.py
@@ -1,19 +1,23 @@
 from collections.abc import Iterator
+from typing import Any
 
 import dagster as dg
 import dlt
+from dagster._core.definitions.result import MaterializeResult
 from dagster_dlt import DagsterDltResource, dlt_assets
 from dagster_dlt.dlt_event_iterator import DltEventType
+from dagster_duckdb import DuckDBResource
 from dlt.destinations.impl.duckdb.configuration import DuckDbCredentials
 
 from cyano_pipeline.defs.dlt.sources.havochvatten import havochvatten_source
 from cyano_pipeline.defs.resources import DUCKDB_PATH
 
+SCHEMA_RAW_HAVOCHVATTEN = "raw_havochvatten"
+SCHEMA_CORE = "core"
+
 DUCKDB_CREDENTIALS: DuckDbCredentials = DuckDbCredentials(
     conn_or_path=str(DUCKDB_PATH),
 )
-
-DATASET_NAME = "raw"
 
 _DLT_DUCKDB_POOL = "duckdb_write"
 
@@ -23,7 +27,7 @@ _DLT_DUCKDB_POOL = "duckdb_write"
     dlt_pipeline=dlt.pipeline(
         pipeline_name="dlt_havochvatten",
         destination=dlt.destinations.duckdb(credentials=DUCKDB_CREDENTIALS),
-        dataset_name=DATASET_NAME,
+        dataset_name=SCHEMA_RAW_HAVOCHVATTEN,
         progress="log",
     ),
     group_name="dlt_havochvatten",
@@ -32,3 +36,40 @@ _DLT_DUCKDB_POOL = "duckdb_write"
 def havochvatten_assets(context: dg.AssetExecutionContext, dlt: DagsterDltResource) -> Iterator[DltEventType]:
     """Loads profiles (SCD2) and results (incremental) from Havochvatten API."""
     yield from dlt.run(context=context)
+
+
+@dg.asset(
+    deps=["dlt_havochvatten_source_profiles"],
+    group_name="core_bathing_waters",
+    pool=_DLT_DUCKDB_POOL,
+    kinds={"duckdb"},
+)
+def dim_bathing_waters(duckdb: DuckDBResource) -> MaterializeResult[Any]:
+    schema = SCHEMA_CORE
+    table = "dim_bathing_waters"
+    fq_table_name = f"{schema}.{table}"
+
+    query = f"""
+CREATE SCHEMA IF NOT EXISTS {schema};
+
+CREATE OR REPLACE TABLE {fq_table_name} AS
+SELECT
+    _waters_id AS id,
+    bathing_water__name AS name
+
+FROM {SCHEMA_RAW_HAVOCHVATTEN}.profiles
+WHERE _dlt_valid_to IS NULL;
+    """
+    with duckdb.get_connection() as conn:
+        _ = conn.execute(query=query)
+
+        row_count = conn.execute(
+            query=f"SELECT COUNT(*) FROM {fq_table_name};",
+        ).fetchone()
+
+        return dg.MaterializeResult(
+            metadata={
+                "row_count": row_count[0] if row_count else None,
+                "table": fq_table_name,
+            },
+        )
